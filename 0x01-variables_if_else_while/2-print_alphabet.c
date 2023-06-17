2a93b359376cbb90d1730e2c9c83588babf9f33d
badrallali <allalibader9@gmail.com>
2023-06-16 17:12:58 -0700

    real

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+* Description: 'check for number if it is positive or negative'
+* Return: always 0 (success)
+*/
+int main(void)
+{
+
+int n = 97;
+while (n <= 122)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
