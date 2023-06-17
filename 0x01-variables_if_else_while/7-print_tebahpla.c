23ce7f84649ee69dbaaeb4623eae974cd799ffc6
badrallali <allalibader9@gmail.com>
2023-06-16 17:47:00 -0700

    real

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 122; ch > 96; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
