5eaa1f534d2bb917830fbe5a4ef4816861a06b20
badrallali <allalibader9@gmail.com>
2023-06-20 17:00:57 -0700

    real

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,14 @@
 
 int main(void)
 {
-	char phrase[9] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i++)
-		_putchar(phrase[i]);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
