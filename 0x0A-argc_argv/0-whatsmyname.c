319b1fef7056c3db1ff7d6e170dcbdc0fe112e57
badrallali <allalibader9@gmail.com>
2023-07-07 07:14:24 -0700

    real

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print its name
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: 0
+ */
+int main(__attribute__ ((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
